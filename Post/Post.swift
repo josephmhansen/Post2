406abeab8455a245bfc44ac56c4ed1e4829403d0
Joseph Hansen <theythemtheir@icloud.com>
2016-08-30 14:10:16 -0600

    implemented model struct and failable initializer

---
+++ Post/Post.swift
@@ -0,0 +1,43 @@
+//
+//  Post.swift
+//  Post
+//
+//  Created by Joseph Hansen on 8/30/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    
+    private let kUsername = "username"
+    private let kText = "text"
+    private let kTimestamp = "timestamp"
+    private let kIdentifier = "identifier"
+    
+    let username: String
+    let text: String
+    let timestamp: NSTimeInterval
+    let identifier: NSUUID
+    
+    
+    init(username: String, text: String, identifier: NSUUID = NSUUID()) {
+        self.username = username
+        self.text = text
+        self.timestamp = NSDate().timeIntervalSince1970
+        self.identifier = identifier
+    }
+    
+    init?(dictionary: [String: AnyObject], identifier: String) {
+        guard let username = dictionary[kUsername] as? String,
+        text = dictionary[kText] as? String,
+        timestamp = dictionary[kTimestamp] as? Double,
+        identifier = NSUUID(UUIDString: identifier) else { return nil }
+        
+        self.username = username
+        self.text = text
+        self.timestamp = NSTimeInterval(floatLiteral: timestamp)
+        self.identifier = identifier
+        
+    }
+}
---
+++ Post/PostController.swift
@@ -0,0 +1,9 @@
+//
+//  PostController.swift
+//  Post
+//
+//  Created by Joseph Hansen on 8/30/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import Foundation
