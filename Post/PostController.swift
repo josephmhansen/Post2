bba30858f5023634eedffbd1fefc6af7022cf9b2
Joseph Hansen <theythemtheir@icloud.com>
2016-08-31 15:45:44 -0600

    completed Project
    working able to fetch and put

--- Post/PostController.swift
+++ Post/PostController.swift
@@ -10,13 +10,15 @@ import Foundation
 
 class PostController {
     
-     static let sharedController = PostController()
+    static let sharedController = PostController()
     
-     let baseURL = NSURL(string: "https://devmtn-post.firebaseio.com/posts")
-     weak var delegate: PostControllerDelegate?
+    static let baseURL = NSURL(string: "https://devmtn-post.firebaseio.com/posts")
+    static let endpoint = baseURL?.URLByAppendingPathExtension("json")
+    
+    weak var delegate: PostControllerDelegate?
     
     
-     var posts: [Post] = [] {
+    var posts: [Post] = [] {
         didSet {
             delegate?.postsUpdated(posts)
         }
@@ -24,46 +26,81 @@ class PostController {
     
     
     init() {
-        fetchPosts { (posts) in
-            guard let posts = posts else { return }
-            self.posts = posts
-        }
+        fetchPosts()
     }
     
-     func fetchPosts(completion: (posts: [Post]?) -> Void) {
-        guard let url = baseURL?.URLByAppendingPathExtension("json") else {
-            print("Error No URL Found")
-            completion(posts: [])
-            return
-        }
+    func addPost(username: String, text: String, completion: ((success: Bool) -> Void)? = nil) {
+        let post = Post(username: username, text: text)
+        guard let requestURL = post.endpoint else {fatalError("URL optional is nil") }
         
-        NetworkController.performRequestForURL(url, httpMethod: .Get) { (data, error) in
+        NetworkController.performRequestForURL(requestURL, httpMethod: .Put, urlParameters: nil, body: post.jsonData) { (data, error) in
+            guard let data = data,
+                responseDataString = NSString(data: data, encoding: NSUTF8StringEncoding) else {
+                    print("no data Found")
+                    return
+            }
+            var success = false
+            defer { completion?(success: success) }
+            if error != nil {
+                print("Error \(error!) : \(responseDataString)")
+            } else {
+                print("Successfully saved data to endpoint")
+                success = true
+            }
+          
+        }
+        fetchPosts()
+        
+    }
+    
+    func fetchPosts(reset reset: Bool = true, completion: ((newPosts: [Post]) -> Void)? = nil) {
+        guard let requestURL = PostController.endpoint else {fatalError("Post Endpoint url failed")}
+        let queryEndInterval = reset ? NSDate().timeIntervalSince1970: posts.last?.timestamp ?? NSDate().timeIntervalSince1970
+        
+        let urlParameters = [
+            "orderBy": "\"timestamp\"",
+            "endAt": "\(queryEndInterval)",
+            "limitToLast" : "15"]
+        
+        
+        NetworkController.performRequestForURL(requestURL, httpMethod: .Get, urlParameters: urlParameters) { (data, error) in
             guard let data = data,
                 responseDataString = NSString(data: data, encoding: NSUTF8StringEncoding) else {
                     print("Error: No Data Found /n\(error?.localizedDescription)")
-                    completion(posts: [])
+                    if let completion = completion {
+                        completion(newPosts: [])
+                    }
                     return
             }
             
             guard let postDictionaries = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)) as? [String: [String: AnyObject]] else {
                 print("Error, Unable to Serialize JSON: \(responseDataString)")
-                completion(posts: [])
+                if let completion = completion {
+                    completion(newPosts: [])
+                }
                 return
             }
             
             let posts = postDictionaries.flatMap({Post(dictionary: $0.1, identifier: $0.0)})
             let sortedPosts = posts.sort({$0.0.timestamp > $0.1.timestamp})
-            self.posts = sortedPosts
-            completion(posts: sortedPosts)
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                if reset {
+                    self.posts = sortedPosts
+                } else {
+                    self.posts.appendContentsOf(sortedPosts)
+                }
+                if let completion = completion {
+                    completion(newPosts: sortedPosts)
+                }
+                return
+            })
+            for post in posts {
+                print(post.text)
+                print(post.queryTimestamp)
+            }
             
             }
-        
         }
-        
-        
-        
-        
-        
 }
 
 protocol PostControllerDelegate: class {
--- Post/PostListTableViewController.swift
+++ Post/PostListTableViewController.swift
@@ -8,90 +8,189 @@
 
 import UIKit
 
-class PostListTableViewController: UITableViewController {
-
+class PostListTableViewController: UITableViewController, PostControllerDelegate {
+    
+    //    @IBOutlet weak var refreshControlOutlet: UIRefreshControl!
+    
+    
+    @IBOutlet weak var refreshControlOutlet: UIRefreshControl!
+    
+    @IBAction func refreshControlPulled(sender: AnyObject) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        postController.fetchPosts { (posts) in
+            sender.endRefreshing()
+            
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        }
+    }
+    @IBAction func addButtonTapped(sender: AnyObject) {
+        presentNewPostAlert()
+    }
+    
+    let postController = PostController()
+    
+    var posts: [Post] = [] {
+        didSet {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.tableView.reloadData()
+                
+            })
+        }
+    }
+    
+    func presentNewPostAlert() {
+        let alertController = UIAlertController(title: "New Post", message: nil, preferredStyle: .Alert)
+        var usernameTextField: UITextField?
+        var messageTextField: UITextField?
+        
+        alertController.addTextFieldWithConfigurationHandler { (usernameField) in
+            usernameField.placeholder = "Display Name"
+            usernameTextField = usernameField
+        }
+        alertController.addTextFieldWithConfigurationHandler { (messageField) in
+            messageField.placeholder = "Enter Text"
+            messageTextField = messageField
+        }
+        
+        let postAction = UIAlertAction(title: "Post", style: .Default) { (action) in
+            guard let username = usernameTextField?.text where !username.isEmpty,
+            let text = messageTextField?.text where !text.isEmpty else {
+                self.presentErrorAlert()
+                return
+            }
+            self.postController.addPost(username, text: text)
+        }
+        alertController.addAction(postAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func presentErrorAlert() {
+        let alertController = UIAlertController(title: "Oh no", message: "Possible network connectivity issues, please try again", preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.row+1 == postController.posts.count {
+            postController.fetchPosts(reset: false, completion: { (newPosts) in
+                if !newPosts.isEmpty {
+                    self.tableView.reloadData()
+                }
+            })
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-PostController.fetchPosts { (posts) in
-    
-        }
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+                postController.delegate = self
+        
+        tableView.estimatedRowHeight = 80
+//        postController.addPost("Joseph", text: "Hello")
+        
+        
     }
-
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    
+    func postsUpdated(posts: [Post]) {
+        tableView.reloadData()
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return postController.posts.count
     }
-
-    /*
+    
+    
+    func formattedStringFromDate(date: NSDate) -> String {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .FullStyle
+        formatter.timeStyle = .ShortStyle
+        
+        return formatter.stringFromDate(date)
+        
+        
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("postCell", forIndexPath: indexPath)
+        
         // Configure the cell...
-
+        
+        let post  = postController.posts[indexPath.row]
+        cell.textLabel?.text = post.text
+        
+        let date = NSDate(timeIntervalSince1970: post.timestamp)
+        cell.detailTextLabel?.text = "\(indexPath.row) - \(post.username) - \(formattedStringFromDate(date)))"
+        
         return cell
     }
-    */
-
+    
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
